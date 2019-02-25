195f3b8d4d28ec72c5cbe11da104dbf5695756c2
jan612 <jan612@cs.bham.ac.uk>
2019-02-25 16:50:31 +0000

    Added a basic skelleton for the fuzzer. The only change that is not reflected in this repository are in the the ble_phy.c file in the nimble repository which allow the fuzzer to intercept any outgoing messages. Later on more changes will have to be made to NimBLE to recieve and feed in the recieved packets to the fuzzer.

--- include/fuzzer.h
+++ include/fuzzer.h
@@ -9,6 +9,20 @@
 #define REPOS_APACHE_MYNEWT_NIMBLE_UOB_BLUETOOTH_INCLUDE_FUZZER_H_
 
 #include <stdint.h>
+#include "log/log.h"
+
+// Logging
+struct log *fuzzer_log;
+#define FUZZER_LOG(lvl, ...) \
+    LOG_ ## lvl(fuzzer_log, LOG_MODULE_PERUSER + 0, __VA_ARGS__)
+
+enum BlePacketType {
+	ADV_IND = 0x0,
+};
+
+void initFuzzer();
+void modifyName(uint8_t *txdata, uint8_t *payload_len);
+void modifyTxBuffer(uint8_t *txdata, uint8_t *payload_len);
 
 
 #endif /* REPOS_APACHE_MYNEWT_NIMBLE_UOB_BLUETOOTH_INCLUDE_FUZZER_H_ */
--- src/fuzzer.c
+++ src/fuzzer.c
@@ -6,5 +6,67 @@
  */
 
 #include "fuzzer.h"
+#include "console/console.h"
 
+void initFuzzer(struct log *logger)
+{
+	fuzzer_log = logger;
+}
 
+void modifyName(uint8_t *txdata, uint8_t *payload_len)
+{
+	//Start of actual string not the segment
+	uint8_t nameStart = 18;
+	uint8_t originalNameLength = 14;
+	// Name string and type
+	uint8_t nameLength = 7;
+	char name[] = {'A', 'L', 'I', ' ', 'Z', 'A', 'B'};
+
+	uint8_t bufferOffset = originalNameLength - nameLength;
+
+	// Set name segment length
+	txdata[nameStart-2] = nameLength + 1;
+
+	// Make space in the buffer if the name is longest than original
+	if(bufferOffset > 0)
+	{
+		for(int i = *payload_len + 2 + (-bufferOffset); i >= nameStart + nameLength; i--)
+		{
+			txdata[i] = txdata[i - (-bufferOffset)];
+		}
+	}
+
+	// Override with new name
+	for(int i = 0; i < nameLength; i++)
+	{
+		txdata[nameStart + i] = name[i];
+	}
+
+	// If name was shorter than original pull up buffer
+	for(int i = nameStart + nameLength; i < *payload_len + 2 - bufferOffset; i++)
+	{
+		txdata[i] = txdata[i + bufferOffset];
+	}
+
+	//set new payload length
+	*payload_len -= bufferOffset;
+	txdata[1] = *payload_len;
+}
+
+void modifyTxBuffer(uint8_t *txdata, uint8_t *payload_len)
+{
+	uint8_t firstByte = txdata[0];
+	uint8_t packetType = firstByte & 15;
+
+	switch(packetType) {
+		case ADV_IND:
+			//Modify name to test
+			modifyName(txdata, payload_len);
+			//int rc = ble_phy_tx(ble_ll_adv_scan_rsp_legacy_pdu_make, advsm, BLE_PHY_TRANSITION_NONE);
+			break;
+
+		default:
+			break;
+	}
+
+}
